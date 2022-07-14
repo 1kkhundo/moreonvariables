7ebbd4ece6cd48fbe08f50f36cb8eae3bfd2c57e
Isaac Wilson <isaacwilson@The-Hidden-Genius-Project-8.local>
2022-07-14 10:29:07 -0700

    first commit

---
+++ specialized/activity1.js
@@ -0,0 +1,8 @@
+let areaSquare,sideSquare; //Statement 1
+
+areaSquare= window.prompt("49.0")
+sideSquare= SquareRoot(areaSquare) 
+console.log (" Square root of ")
+console.log  (areaSquare) 
+console.log  (" is ")
+console.log  (sideSquare)
---
+++ specialized/acvitiy4.js
@@ -0,0 +1,8 @@
+let RandomValue 
+RandomValue = RandomNumber(1,6)
+console. log (RandomValue)
+RandomValue = RandomNumber (1,6)
+console. log (RandomValue)
+RandomValue = RandomNumber (1,6) 
+console. log  (RandomValue)
+
